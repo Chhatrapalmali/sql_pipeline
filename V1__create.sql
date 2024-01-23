849423b82542c11182b9f86ef3fba2cd3843dc4f
Chhatrapalmali <125431784+Chhatrapalmali@users.noreply.github.com>
2024-01-23 11:12:27 +0530

    Update V1__create.sql

--- V1__create.sql
+++ V1__create.sql
@@ -1,4 +1,4 @@
-DELETE FROM flyway_schema_history  WHERE version=1.12;
+DELETE FROM flyway_schema_history  WHERE version IS NOT NULL;
 create table PERSONAL (
     ID int not null,
     NAME_time varchar(100) not null
