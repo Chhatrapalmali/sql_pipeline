74d713d9f1b848d9a4efff91b96159705371d6b6
Chhatrapalmali <125431784+Chhatrapalmali@users.noreply.github.com>
2024-01-23 11:10:16 +0530

    Delete V1.12__create.sql

--- V1.12__create.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM flyway_schema_history  WHERE version=1.2;
-DELETE FROM flyway_schema_history  WHERE version=1.3;
-create table PERSON6 (
-    ID int not null,
-    NAME_time varchar(100) not null
-);
